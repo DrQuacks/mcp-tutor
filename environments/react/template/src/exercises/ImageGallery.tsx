0529e9c4952c3c3adbd2e91ce49247904afb009f
Michael Kellar <michael.d.kellar@gmail.com>
2025-12-06 14:13:24 -0800

    make exercise creation more aware of what tests will need to select stuff

--- environments/react/template/src/exercises/ImageGallery.tsx
+++ environments/react/template/src/exercises/ImageGallery.tsx
@@ -14,13 +14,53 @@
 
 import { useState } from 'react'
 
+type ImageElement = {id:number,url:string,title:string}
 function ImageGallery() {
+  const [url,setUrl] = useState("")
+  const [title,setTitle] = useState("")
+  const [imageList,setImageList] = useState<ImageElement[]>([])
+  const [newId,setNewId] = useState(0)
+
+  const addImage = () => {
+    const newImage:ImageElement = {id:newId,url,title}
+    setImageList(prev => [newImage,...prev])
+    setUrl("")
+    setTitle("")
+    setNewId(prev => prev+1)
+  }
+
+  const removeImage = (id:number) => {
+    setImageList(prev => {
+      const newList:ImageElement[] = prev.filter(img => {
+        return (img.id !== id)
+      })
+      return newList
+    })
+  }
   return (
     <div>
       <h2>Image Gallery</h2>
+      <div>
+        <label>Image URL: </label>
+        <input value={url} onChange={(e)=>{setUrl(e.currentTarget.value)}}/>
+      </div>
+      <div>
+        <label>Image Title: </label>
+        <input value={title} onChange={(e)=>{setTitle(e.currentTarget.value)}}/>
+      </div>
+      <button onClick={addImage}>Add Image</button>
       
       <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '1rem', marginTop: '2rem' }}>
         {/* Your code here */}
+        {imageList.map(im => {
+          return (
+            <div key={im.id}>
+              <img width="100" height="100" src={im.url}/>
+              <p>{im.title}</p>
+              <button onClick={() => {removeImage(im.id)}}>Delete</button>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
