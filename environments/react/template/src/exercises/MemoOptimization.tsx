55dcb5b40133ef4051f22901afd56c60baaee92e
Michael Kellar <michael.d.kellar@gmail.com>
2025-12-26 23:15:54 -0800

    more tutorial progress

--- environments/react/template/src/exercises/MemoOptimization.tsx
+++ environments/react/template/src/exercises/MemoOptimization.tsx
@@ -1,8 +1,16 @@
-import { useState , useMemo } from 'react';
+import { useState , useMemo, useEffect } from 'react';
 
 function MemoOptimization() {
   const [numbers,setNumbers] = useState([1,2,3,4,5])
   const [count,setCount] = useState(0)
+  const [userId,setUserId] = useState(1)
+  const [renderCount,setRenderCount] = useState(0)
+  const user = useMemo(()=>{
+    return {id:userId, name: 'User '+userId}
+  },[userId])
+  useEffect(()=>{
+    console.log('User object changed: '+JSON.stringify(user))
+  },[user])
   const sum = useMemo(() => {
     return (numbers.reduce((acc,num) => {
     console.log('Calculating Sum')
@@ -13,8 +21,12 @@ function MemoOptimization() {
       <h2>Memoization Optimization</h2>
       <button onClick={()=>setCount(prev=>prev+1)}>Increment Count</button>
       <button onClick={()=>setNumbers(prev=>([...prev,Math.floor(Math.random()*100)]))}>Add Number</button>
+      <button onClick={()=>setRenderCount(prev=>prev+1)}>Trigger Render</button>
+      <button onClick={()=>setUserId(prev=>prev+1)}>Change User</button>
       <p>Count is: {count}</p>
       <p>Sum is: {sum}</p>
+      <p>Render count is: {renderCount}</p>
+      <p>Username is: {user.name}</p>
     </div>
   );
 }
