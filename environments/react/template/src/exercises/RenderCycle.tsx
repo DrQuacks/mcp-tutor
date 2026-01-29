9b1f02ecc351b104715464aa6190ec2c0d2b9432
Michael Kellar <michael.d.kellar@gmail.com>
2026-01-29 15:27:02 -0800

    update user progress/internal issues

--- environments/react/template/src/exercises/RenderCycle.tsx
+++ environments/react/template/src/exercises/RenderCycle.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState , useEffect } from 'react';
 
 const Child = () => {
   console.log('Child Rendered')
@@ -9,13 +9,23 @@ const Child = () => {
 function RenderCycle() {
   console.log('Component Rendered')
   const [counter,setCounter] = useState(0)
+  const [renderCount,setRenderCount] = useState(0)
+  console.log('RENDER PHASE: Computing JSX, renderCount = ' + renderCount)
+  useEffect(()=>{
+    console.log("COMMIT PHASE: DOM updated")
+  })
+  useEffect(()=>{
+    console.log("COMMIT PHASE: Initial mount only")
+  },[])
 
   return (
     <div>
       <h2>React Render Cycle</h2>
       <button onClick={()=>setCounter(prev=>prev+1)}>Raise Count</button>
+      <button onClick={()=>setRenderCount(prev=>prev+1)}>Raise Render Count</button>
       <Child/>
       <p>Parent Count is: {counter}</p>
+      <p>Render Count is: {renderCount}</p>
     </div>
   );
 }
