a658540fc43d13c40b48d90966c8b4fc41a0bd64
Michael Kellar <michael.d.kellar@gmail.com>
2026-02-27 22:07:18 -0800

    tutorial progress

--- environments/react/template/src/exercises/DummyTodoCrudAutocomplete.tsx
+++ environments/react/template/src/exercises/DummyTodoCrudAutocomplete.tsx
@@ -1,4 +1,5 @@
 import {useState,useEffect} from 'react'
+import React from 'react'
 
 type Todos = {
     id: number;
@@ -10,23 +11,36 @@ export default function DummyTodoCrudAutocomplete() {
     const [isLoading,setIsLoading] = useState(false)
     const [error,setError] = useState<Error|null>(null)
     const [userText,setUserText] = useState("")
-    useEffect(()=>{
-        const getData = async () => {
-            setIsLoading(true)
-            setError(null)
-            try {
-                const response = await fetch('http://localhost:4000/api/todos')
-                if (!response.ok) throw new Error('Data fetch failed')
-                const data = await response.json() as Todos[]
-                setList(data)
-            } catch (err:unknown) {
-                setError(err as Error)
-            }
-            setIsLoading(false)
+    const getData = async () => {
+        setIsLoading(true)
+        setError(null)
+        try {
+            const response = await fetch('http://localhost:4000/api/todos')
+            if (!response.ok) throw new Error('Data fetch failed')
+            const data = await response.json() as Todos[]
+            setList(data)
+        } catch (err:unknown) {
+            setError(err as Error)
         }
+        setIsLoading(false)
+    }
+    useEffect(()=>{
+        // const getData = async () => {
+        //     setIsLoading(true)
+        //     setError(null)
+        //     try {
+        //         const response = await fetch('http://localhost:4000/api/todos')
+        //         if (!response.ok) throw new Error('Data fetch failed')
+        //         const data = await response.json() as Todos[]
+        //         setList(data)
+        //     } catch (err:unknown) {
+        //         setError(err as Error)
+        //     }
+        //     setIsLoading(false)
+        // }
         getData()
     },[])
-    const handleSubmit = async (e:React.MouseEvent) => {
+    const handleSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         try {
             const res = await fetch('http://localhost:4000/api/todos',{
@@ -37,15 +51,56 @@ export default function DummyTodoCrudAutocomplete() {
             if (!res.ok) throw new Error(`Post error ${res.status}`)
             const data = await res.json() as Todos
             setList(prev => [...prev,data])
+            setUserText("")
         } catch(err:unknown) {
             setError(err as Error)
         }
     }
+    const toggleCompletion = async (id:number,completed:boolean) => {
+        setIsLoading(true)
+        setError(null)
+        try {
+            const res = await fetch(`http://localhost:4000/api/todos/${id}`,{
+                method:'PATCH',
+                headers:{"Content-Type":"application/json"},
+                body:JSON.stringify({completed:!completed})
+            })
+            if(!res.ok) throw new Error(`Update error ${res.status}`)
+            const data = await res.json() as Todos
+            setList(prev => {
+                return (prev.map(todo => {
+                    if (todo.id !== id) return todo
+                    else return data
+                }))
+            })
+        } catch (e:unknown) {
+            setError(e as Error)
+        } finally {
+            setIsLoading(false)
+        }
+    }
+    const handleDelete = async (id:number) => {
+        
+    }
   return (
     <div>
       <h2>Todo CRUD with Autocomplete</h2>
-      <input type='text' value={userText} onChange={(e)=>setUserText(e.currentTarget.value)}/>
-      <button onClick={handleSubmit}>Submit</button>
+      <li>
+        {list.map((todo,index) => {
+            const completeText = todo.completed ? "Task Complete!" : "In Progress"
+            return (
+                <li key = {todo.id}>
+                    <p>{todo.title} | {completeText}</p>
+                    <button onClick={() => toggleCompletion(todo.id,todo.completed)} disabled={todo.completed}>Mark Complete</button>
+                    <button onClick={() => handleDelete(todo.id)}>Delete</button>
+                </li>
+            )
+        })}
+      </li>
+      <form onSubmit={handleSubmit}>
+        <input type='text' value={userText} onChange={(e)=>setUserText(e.currentTarget.value)}/>
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   );
 }
